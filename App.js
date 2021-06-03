0bed03410e0c3e6affdfb9c68bf910fd4b22521e
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-04 01:53:02 +0600

    update main component

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-=======
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
-import Home from './components/Home/Home';
->>>>>>> dafefd6beab766b3e473ce32dbd82748e85a94f7
+import { StyleSheet, Text, SafeAreaView, ScrollView } from "react-native";
+import Home from "./components/Home/Home";
 
 export default function App() {
   return (
