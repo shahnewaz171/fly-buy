986e47325ec9926232e6990132bb66b54d8c781a
svn5969 <sourav.svn.10@gmail.com>
2021-06-04 23:48:45 +0600

    fix issue1

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 export default function Footer() {
     return (
         <View style={styles.footer}>
+            {/* for opacity */}
             <TouchableOpacity>
             <Text style={{fontSize:30}}>Text</Text>
+
             </TouchableOpacity>
             
         </View>
